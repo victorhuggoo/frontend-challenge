3a227f6a1cf6b5645c6325f64d0261b2a72de0ab
Victor Huggo <victorhuggo97@gmail.com>
2020-08-30 16:44:54 -0300

    api ready

--- services/api.js
+++ services/api.js
@@ -1,5 +1,24 @@
 const urlApi = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
+
 fetch(urlApi)
-    .then(function(response) {
-        return console.log(response.json());
-    })
+    .then(response =>
+        response.json()
+    ).then(data => {
+        var dynamicProducts;
+
+        var apiResponse = data.products[0];
+
+        dynamicProducts += `
+            <img src=${apiResponse.image} alt="">
+            <strong>${apiResponse.name}</strong>
+            <p>${apiResponse.description}</p>
+
+            <p>de: R$:${apiResponse.oldPrice}</p>
+            <strong>por: R$ ${apiResponse.price}</strong>
+            <p>2x de 78</p>
+
+            <button>comprar</button>
+            `;
+        document.getElementById('teste').innerText(dynamicProducts)
+
+    });
